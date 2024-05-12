90f9fde95d4caae31d9d7f94ecd49ff69371dc8e
Nadir Muzaffar <nadirabid@users.noreply.github.com>
2024-05-11 18:21:14 -0700

    check

---
+++ Assets/Scripts/AvatarClipVideoPlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class AvatarClipVideoPlayerController : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();   
+    }
+
+    private List<GameObject> woodprites = new List<GameObject>();
+
+    void Update()
+    {
+        Debug.Log($"Nadir: Current timestamp: {videoPlayer.time}");
+
+        if (videoPlayer.time >= 5f && videoPlayer.time <= 15f)
+        {
+            SpawnWoodprites();
+        }
+        else if (videoPlayer.time > 200f)
+        {
+            DestroyWoodprites();
+        }
+    }
+
+    void SpawnWoodprites()
+    {
+        if (woodprites.Count == 0)
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                GameObject woodprite = Instantiate(Resources.Load<GameObject>("WoodspriteTextured"), transform.position, Quaternion.identity);
+                woodprites.Add(woodprite);
+            }
+        }
+    }
+
+    void DestroyWoodprites()
+    {
+        foreach (GameObject woodprite in woodprites)
+        {
+            Destroy(woodprite);
+        }
+        woodprites.Clear();
+    }
+}
